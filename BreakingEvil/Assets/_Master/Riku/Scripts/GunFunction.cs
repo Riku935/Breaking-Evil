adb61e3be00bf5dbc5b6f000a1f624bccda31482
Riku935 <exodo302.richy@gmail.com>
2023-03-07 16:53:25 -0600

    Scripts

--- BreakingEvil/Assets/_Master/Riku/Scripts/GunFunction.cs
+++ BreakingEvil/Assets/_Master/Riku/Scripts/GunFunction.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunFunction : MonoBehaviour
+public class GunFunction : Object_Interaction
 {
     public GunData _gunData;
+
+    [Header("Bullet Data")]
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed;
+    private GameObject playerRecoil;
+
     void Start()
     {
         _gunData.currentCharger = _gunData.chargerSize;
@@ -16,13 +20,21 @@ public class GunFunction : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetMouseButton(0) && _gunData.canShoot && _gunData.currentCharger > 0)
+        Reload();
+    }
+
+    void Shoot()
+    {
+        if ( _gunData.canShoot && _gunData.currentCharger > 0)
         {
             _gunData.canShoot = false;
             _gunData.currentCharger--;
             StartCoroutine(ShootGun());
         }
-        else if (Input.GetKeyDown(KeyCode.R) && _gunData.currentCharger < _gunData.chargerSize && _gunData.reservedAmmo > 0)
+    }
+    void Reload()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && _gunData.currentCharger < _gunData.chargerSize && _gunData.reservedAmmo > 0)
         {
             int ammoNeeded = _gunData.chargerSize - _gunData.currentCharger;
             if (ammoNeeded >= _gunData.currentReserved)
@@ -37,22 +49,27 @@ public class GunFunction : MonoBehaviour
             }
         }
     }
-
-    void Recoil()
-    {
-        transform.localPosition -= Vector3.forward * 0.1f;
-    }
     void Fire()
     {
         var bullet = Instantiate(bulletPrefab,bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
     }
-
+    void Recoil()
+    {
+        //transform.position -= Vector3.forward * 0.1f;
+    }
     IEnumerator ShootGun()
     {
         Recoil();
         Fire();
         yield return new WaitForSeconds(_gunData.fireRate);
         _gunData.canShoot = true;
+    }
+
+    public override void Use()
+    {
+        base.Use();
+        Shoot();
+        
     }
 }
