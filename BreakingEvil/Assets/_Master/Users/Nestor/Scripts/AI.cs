67d1b3cfae5f687628200ef5ba9f66235d8ca9f5
LDom890 <montecinonestor890@gmail.com>
2023-03-10 12:36:59 -0600

    Add AI

---
+++ BreakingEvil/Assets/_Master/Users/Nestor/Scripts/AI.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+
+    public NavMeshAgent navMeshAgent;
+    public Transform[] destinations;
+    private int i = 0;
+
+    [Header("FollowPlayer----------------------")]
+    
+    public bool followPlayer;
+    private GameObject player;
+    [SerializeField] private float distanceToPlayer;
+    [SerializeField] private float distanceToFollowPlayer=3f;
+    [SerializeField] private float distanceToFollowPath = 2f;
+
+
+
+    void Start()
+    {
+        navMeshAgent.destination = destinations[0].transform.position;
+        player = FindObjectOfType<Player_Controller>().gameObject;
+    }
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if(distanceToPlayer <= distanceToFollowPlayer && followPlayer)
+        {
+            FollowPlayer();
+        }
+        else
+        {
+            EnemyPath();
+        }
+    }
+    public void EnemyPath()
+    {
+        navMeshAgent.destination = destinations[i].position;
+        if(Vector3.Distance(transform.position,destinations[i].position)<=distanceToFollowPath)
+        {
+            if(destinations[i] != destinations[destinations.Length -1])
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+        }
+    }
+    public void FollowPlayer()
+    {
+        navMeshAgent.destination = player.transform.position;
+    }
+}
