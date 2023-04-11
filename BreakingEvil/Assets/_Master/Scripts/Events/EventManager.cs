0c27c6ac135832e9944b8c5a246217d2f7805b81
Riku935 <exodo302.richy@gmail.com>
2023-04-11 17:15:52 -0600

    Puzzle

--- BreakingEvil/Assets/_Master/Scripts/Events/EventManager.cs
+++ BreakingEvil/Assets/_Master/Scripts/Events/EventManager.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventManager : MonoBehaviour
 {
-    
+    //[SerializeField] private UnityEvent puzzle;
+
+    public float rotationSpeed = 10f;
+    private void Start()
+    {
+        TestPuzzle temp = new TestPuzzle();
+        temp.OnPuzzleComplete += HandleOnPuzzleComplete;
+    }
+    void HandleOnPuzzleComplete(GameObject sender)
+    {
+        print(sender.name);
+    }
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
 }
