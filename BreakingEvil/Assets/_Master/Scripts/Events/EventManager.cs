5f184a89df32aff489f9186ee66b8a5e02ea4e79
unknown <exodo302.richy@gmail.com>
2023-04-13 12:17:59 -0500

    Escena Tutorial

--- BreakingEvil/Assets/_Master/Scripts/Events/EventManager.cs
+++ BreakingEvil/Assets/_Master/Scripts/Events/EventManager.cs
@@ -12,16 +12,26 @@ public class EventManager : MonoBehaviour
     public float rotationSpeed = 10f;
     public bool canOpen = false;
     public int puzzleCount;
+    private Animator anim;
     private void Awake()
     {
         obj = this;
+    }
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
     }
     private void Update()
     {
         if (puzzleCount == 3)
         {
-            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            MainDoorEvent();
         }
+    }
+
+    void MainDoorEvent()
+    {
+        anim.Play("OpenDoor");
     }
     private void OnDestroy()
     {
