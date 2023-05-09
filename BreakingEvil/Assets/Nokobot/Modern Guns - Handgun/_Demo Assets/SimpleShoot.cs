29b8243df7e253a531c27cb1494860de3bc7e4df
Tobias Gtz <perepump@gmail.com>
2023-05-09 16:51:23 -0600

    Arma Vr

--- BreakingEvil/Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
+++ BreakingEvil/Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [AddComponentMenu("Nokobot/Modern Guns/Simple Shoot")]
 public class SimpleShoot : MonoBehaviour
 {
+    public PistolData pistolRef;
+
     [Header("Prefab Refrences")]
     public GameObject bulletPrefab;
     public GameObject casingPrefab;
     public GameObject muzzleFlashPrefab;
+    public TextMeshPro bulletText;
 
-    [Header("Location Refrences")]
+    [Header("References")]
     [SerializeField] private Animator gunAnimator;
     [SerializeField] private Transform barrelLocation;
     [SerializeField] private Transform casingExitLocation;
 
     [Header("Settings")]
-    [Tooltip("Specify time to destory the casing object")] [SerializeField] private float destroyTimer = 2f;
-    [Tooltip("Bullet Speed")] [SerializeField] private float shotPower = 500f;
-    [Tooltip("Casing Ejection Speed")] [SerializeField] private float ejectPower = 150f;
+    [Tooltip("Tiempo para destruir el casquillo")][SerializeField] private float destroyTimer = 2f;
+    [Tooltip("velocidad de la bala")][SerializeField] private float shotPower = 500f;
+    [Tooltip("velocidad de salida del cartucho")][SerializeField] private float ejectPower = 150f;
 
     public AudioSource source;
     public AudioClip fireSound;
@@ -29,63 +33,65 @@ public class SimpleShoot : MonoBehaviour
 
         if (gunAnimator == null)
             gunAnimator = GetComponentInChildren<Animator>();
+
+        pistolRef.currentAmmo = pistolRef.maxAmmo;
     }
 
     void Update()
     {
-        ////If you want a different input, change it here
+        bulletText.text = pistolRef.currentAmmo.ToString(); 
         //if (Input.GetButtonDown("Fire1"))
         //{
-        //    //Calls animation on the gun that has the relevant animation events that will fire
         //    gunAnimator.SetTrigger("Fire");
         //}
     }
-
+    [ContextMenu("Disparo")]
     public void PullTheTrigger()
     {
-        gunAnimator.SetTrigger("Fire");
+        if (pistolRef.currentAmmo > 0)
+        {
+            gunAnimator.SetTrigger("Fire");
+        }
     }
 
-    //This function creates the bullet behavior
+
+
     void Shoot()
     {
-        source.PlayOneShot(fireSound); 
+        source.PlayOneShot(fireSound);
+        pistolRef.currentAmmo--;
         if (muzzleFlashPrefab)
         {
-            //Create the muzzle flash
             GameObject tempFlash;
             tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
 
-            //Destroy the muzzle flash effect
             Destroy(tempFlash, destroyTimer);
         }
 
-        //cancels if there's no bullet prefeb
+
         if (!bulletPrefab)
         { return; }
 
-        // Create a bullet and add force on it in direction of the barrel
         Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
 
-    }
+    } 
 
-    //This function creates a casing at the ejection slot
     void CasingRelease()
     {
-        //Cancels function if ejection slot hasn't been set or there's no casing
         if (!casingExitLocation || !casingPrefab)
         { return; }
 
-        //Create the casing
         GameObject tempCasing;
         tempCasing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
-        //Add force on casing to push it out
         tempCasing.GetComponent<Rigidbody>().AddExplosionForce(Random.Range(ejectPower * 0.7f, ejectPower), (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
-        //Add torque to make casing spin in random direction
         tempCasing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(100f, 1000f)), ForceMode.Impulse);
 
-        //Destroy casing after X seconds
         Destroy(tempCasing, destroyTimer);
+    }
+
+    void Reload()
+    {
+        pistolRef.currentAmmo = pistolRef.maxAmmo;
     }
 
 }
