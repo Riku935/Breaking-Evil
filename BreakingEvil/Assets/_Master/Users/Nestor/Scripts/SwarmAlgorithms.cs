2b272e5c03cdca4cf97a4f223f39dcb2f91d4d10
Tobias Gtz <perepump@gmail.com>
2023-05-23 16:44:24 -0600

    Zombie

--- BreakingEvil/Assets/_Master/Users/Nestor/Scripts/SwarmAlgorithms.cs
+++ BreakingEvil/Assets/_Master/Users/Nestor/Scripts/SwarmAlgorithms.cs
@@ -9,6 +9,17 @@ public class SwarmAlgorithms : MonoBehaviour
     public Transform[] destinations;
     private int i = 0;
 
+  
+    private Animator anim;
+
+
+    [Header("Attack Values")]
+    [SerializeField] private bool _isAttack = false;
+    [SerializeField] private float _distanceToAttack = 1;
+    [SerializeField] private float _attackTime = 1.5f;
+
+
+
     [Header("FollowPlayer----------------------")]
     [SerializeField] private bool followPlayer = false;
     private GameObject player;
@@ -20,6 +31,8 @@ public class SwarmAlgorithms : MonoBehaviour
 
     void Start()
     {
+        anim = GetComponent<Animator>();
+
         //navMeshAgent.destination = destinations[0].transform.position;
         player = FindObjectOfType<PlayerControllerTarget>().gameObject;
 
@@ -29,9 +42,10 @@ public class SwarmAlgorithms : MonoBehaviour
 
     void Update()
     {
+      
         distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
-        if (distanceToPlayer <= distanceToFollowPlayer)
+        if (distanceToPlayer <= distanceToFollowPlayer && distanceToPlayer > _distanceToAttack)
         {
             FollowPlayer();
             followPlayer = true;
@@ -42,15 +56,22 @@ public class SwarmAlgorithms : MonoBehaviour
                 FollowPlayerGroup();
             }
         }
+        else if (distanceToPlayer <= _distanceToAttack)
+        {
+            followPlayer = false;
+            navMeshAgent.speed = 0;
+            StartCoroutine(Attack());
+        }
         else
         {
             EnemyPath();
+            navMeshAgent.speed = 3;
         }
     }
 
     public void EnemyPath()
     {
-        navMeshAgent.destination = destinations[i].position;
+         navMeshAgent.destination = destinations[i].position;
         if (Vector3.Distance(transform.position, destinations[i].position) <= distanceToFollowPath)
         {
             if (destinations[i] != destinations[destinations.Length - 1])
@@ -62,11 +83,15 @@ public class SwarmAlgorithms : MonoBehaviour
                 i = 0;
             }
         }
+
+        anim.Play("Z_Walk_InPlace");
     }
 
     public void FollowPlayer()
     {
+        navMeshAgent.speed = 8;
         navMeshAgent.destination = player.transform.position;
+        anim.Play("Z_Run_InPlace");
     }
 
     public void FollowPlayerGroup()
@@ -83,5 +108,18 @@ public class SwarmAlgorithms : MonoBehaviour
     public void StopEnemyPath()
     {
         navMeshAgent.speed = 0;
+    }
+
+   
+
+    public IEnumerator Attack()
+    {
+        anim.Play("Z_Attack");
+        yield return new WaitForSecondsRealtime(_attackTime);
+        StopCoroutine(Attack());
+        if (distanceToPlayer>= _distanceToAttack)
+        {
+            followPlayer = true;
+        }
     }
 }
