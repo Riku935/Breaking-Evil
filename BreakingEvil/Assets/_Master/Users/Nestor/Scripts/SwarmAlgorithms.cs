1d9d16006eda3d11ac9e329dd70a0c4c9975d2e5
LDom890 <montecinonestor890@gmail.com>
2023-05-26 13:57:15 -0600

    Add Complete AI

--- BreakingEvil/Assets/_Master/Users/Nestor/Scripts/SwarmAlgorithms.cs
+++ BreakingEvil/Assets/_Master/Users/Nestor/Scripts/SwarmAlgorithms.cs
@@ -9,16 +9,12 @@ public class SwarmAlgorithms : MonoBehaviour
     public Transform[] destinations;
     private int i = 0;
 
-  
     private Animator anim;
-
 
     [Header("Attack Values")]
     [SerializeField] private bool _isAttack = false;
     [SerializeField] private float _distanceToAttack = 1;
     [SerializeField] private float _attackTime = 1.5f;
-
-
 
     [Header("FollowPlayer----------------------")]
     [SerializeField] private bool followPlayer = false;
@@ -27,33 +23,43 @@ public class SwarmAlgorithms : MonoBehaviour
     [SerializeField] private float distanceToFollowPlayer = 3f;
     [SerializeField] private float distanceToFollowPath = 2f;
 
-    private SwarmAlgorithms[] groupMembers; // Array para almacenar los agentes del mismo grupo
+    private SwarmAlgorithms leader; // Referencia al agente líder que tiene followPlayer en true
 
     void Start()
     {
         anim = GetComponent<Animator>();
 
-        //navMeshAgent.destination = destinations[0].transform.position;
         player = FindObjectOfType<PlayerControllerTarget>().gameObject;
 
         // Obtener todos los agentes con el mismo script SwarmAlgorithms
-        groupMembers = FindObjectsOfType<SwarmAlgorithms>();
+        SwarmAlgorithms[] groupMembers = FindObjectsOfType<SwarmAlgorithms>();
+
+        // Buscar el agente líder en el grupo
+        foreach (SwarmAlgorithms member in groupMembers)
+        {
+            if (member.followPlayer)
+            {
+                leader = member;
+                break;
+            }
+        }
     }
 
     void Update()
     {
-      
         distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
         if (distanceToPlayer <= distanceToFollowPlayer && distanceToPlayer > _distanceToAttack)
         {
-            FollowPlayer();
             followPlayer = true;
+            FollowPlayer();
 
-            // Si este agente está siguiendo al jugador, hacer que los demás agentes del grupo también lo sigan
+            // Si este agente está siguiendo al jugador, establecerlo como líder y hacer que los demás agentes lo sigan
             if (followPlayer)
             {
+                leader = this;
                 FollowPlayerGroup();
+                distanceToFollowPlayer *= 2; // Duplicar la distancia de seguimiento al jugador
             }
         }
         else if (distanceToPlayer <= _distanceToAttack)
@@ -64,14 +70,16 @@ public class SwarmAlgorithms : MonoBehaviour
         }
         else
         {
+            followPlayer = false;
             EnemyPath();
             navMeshAgent.speed = 3;
+            distanceToFollowPlayer /= 2; // Restaurar la distancia de seguimiento al jugador
         }
     }
 
     public void EnemyPath()
     {
-         navMeshAgent.destination = destinations[i].position;
+        navMeshAgent.destination = destinations[i].position;
         if (Vector3.Distance(transform.position, destinations[i].position) <= distanceToFollowPath)
         {
             if (destinations[i] != destinations[destinations.Length - 1])
@@ -96,11 +104,14 @@ public class SwarmAlgorithms : MonoBehaviour
 
     public void FollowPlayerGroup()
     {
-        foreach (SwarmAlgorithms member in groupMembers)
+        if (leader == null)
+            return;
+
+        foreach (SwarmAlgorithms member in FindObjectsOfType<SwarmAlgorithms>())
         {
-            if (member != this) // No aplicar al agente actual
+            if (member != leader) // No aplicar al líder actual
             {
-                member.navMeshAgent.destination = player.transform.position;
+                member.navMeshAgent.destination = leader.transform.position;
             }
         }
     }
@@ -110,14 +121,12 @@ public class SwarmAlgorithms : MonoBehaviour
         navMeshAgent.speed = 0;
     }
 
-   
-
     public IEnumerator Attack()
     {
         anim.Play("Z_Attack");
         yield return new WaitForSecondsRealtime(_attackTime);
         StopCoroutine(Attack());
-        if (distanceToPlayer>= _distanceToAttack)
+        if (distanceToPlayer >= _distanceToAttack)
         {
             followPlayer = true;
         }
